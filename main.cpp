6011985eb29a9a40d022731daf48905901e427e5
Master_Procrastinator <69257481+Croionxx@users.noreply.github.com>
2025-01-24 22:52:56 +0530

    Added window box

--- main.cpp
+++ main.cpp
@@ -1,13 +1,40 @@
-#include <iostream>
-#include <string>
-using namespace std;
+#include <sys/ioctl.h>
+#include <stdio.h>
+#include <unistd.h>
 
-
-int main() {
-    string fullName;
-    cout << "Type your full name: ";
-    getline (cin, fullName);
-    cout << "Your name is: " << fullName;   
+int box(int rows, int cols){
+    for (int i = 0; i < rows; i++){
+        for (int j = 0; j < cols; j++){
+            if (i == 0 || i == rows - 1){
+                if (j == 0 || j == cols - 1){
+                    printf("+");
+                } else {
+                    printf("-");
+                }
+            } else {
+                if (j == 0 || j == cols - 1){
+                    printf("|");
+                } else {
+                    printf(" ");
+                }
+            }
+        }
+        printf("\n");
+    }
     return 0;
 }
 
+
+
+
+
+
+int main (int argc, char **argv)
+{
+    struct winsize w;
+    ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+
+    box(w.ws_row-5, w.ws_col-5);
+
+    return 0; 
+}
