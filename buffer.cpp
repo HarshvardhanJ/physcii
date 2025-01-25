0edbe62a1f217b506fac1478e4e007f83a334c97
HarshvardhanJ <harshvardhanjp@gmail.com>
2025-01-25 20:55:21 +0530

    ehh

--- buffer.cpp
+++ buffer.cpp
@@ -5,13 +5,11 @@
 #include <sys/ioctl.h>
 
 int main() {
-    // Initialize ncurses
     initscr();
     cbreak();
     noecho();
     curs_set(0);
 
-    // Dimensions of the box on the screen
 
     struct winsize w;
     ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
@@ -19,22 +17,17 @@ int main() {
     int width  = w.ws_col;
     int height = w.ws_row;
 
-    // Define the path for the square (y,x) positions
     std::vector<std::pair<int,int>> path = {
         {3, 3}, {3, 50}, {3, 20}, {8, 20}, {8, 10}, {8, 3}
     };
 
-    // Size of the moving square
     int squareSize = 3;
 
-    // Time delay between frames (microseconds)
     int delayUs = 300000;
 
-    // Go through the array once
     for (auto &pos : path) {
         clear();
 
-        // Draw a simple box border
         for (int x = 0; x < width; x++) {
             mvaddch(0, x, '-');
             mvaddch(height - 1, x, '-');
@@ -44,20 +37,17 @@ int main() {
             mvaddch(y, width - 1, '|');
         }
 
-        // Draw the square in the new position
         for (int row = 0; row < squareSize; row++) {
             for (int col = 0; col < squareSize; col++) {
                 mvaddch(pos.first + row, pos.second + col, '#');
             }
         }
 
-        // Refresh to display changes
         refresh();
 
         usleep(delayUs);
     }
 
-    // Terminate ncurses mode
     endwin();
     return 0;
 }
